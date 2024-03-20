d63e7533e6e07d036ada8b95881fd11aae21bc9f
Abduvohid Ergashev <Ab.ergashev@anorbank.org>
2024-03-20 16:18:26 +0500

    cloud config server

---
+++ src/test/java/uz/gc/alator/config/server/AlatorConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.gc.alator.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlatorConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
